7828edd05970fb043217b4f62d369130368d2c80
Ryan Bagwell <ryan@ryanbagwell.com>
2016-07-19 13:01:51 -0400

    adding livereload programmically

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Meter from './lib/components/Meter';
 import conf from './conf/config';
+import loadscript from 'scriptjs';
 
 
 export default class Dashboard extends React.Component {
@@ -25,3 +26,8 @@ export default class Dashboard extends React.Component {
 let dashBoard = <Dashboard />;
 
 ReactDOM.render(dashBoard, document.getElementsByTagName('main')[0]);
+
+
+if (DEBUG) {
+  loadscript(`http://${(location.host || 'localhost').split(':')[0]}:35729/livereload.js?snipver=1`);
+}
